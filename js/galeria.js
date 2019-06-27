23fe2c846e942eb1a9bc65470b11dd6859685119
Anderson Garcia <andersongarciapinheiro@gmail.com>
2019-06-27 19:14:03 -0300

    gallery code optmized kendy helps

--- js/galeria.js
+++ js/galeria.js
@@ -1,4 +1,3 @@
-let noHit = document.querySelector(".noHit");
 let bgImg1Full = document.querySelector(".bgImg1Full");
 let img1Full = document.querySelector(".img1Full");
 let img1 = document.querySelector(".img1");
@@ -6,10 +5,8 @@ let img1 = document.querySelector(".img1");
 
 img1.onclick = function() {
   bgImg1Full.style.zIndex = "1"
-  noHit.style.zIndex = "9999"
 }
 
 bgImg1Full.onclick = function() {
   bgImg1Full.style.zIndex = "-1"
-  noHit.style.zIndex = "-1"
 }
