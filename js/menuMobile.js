f6b6301d19c9e7ef841d3dbf94613c4b488df309
Anderson Garcia <andersongarciapinheiro@gmail.com>
2019-07-04 17:50:20 -0300

    menu auto resize

--- js/menuMobile.js
+++ js/menuMobile.js
@@ -1,17 +1,30 @@
 const iconMenu = document.querySelector('#nav-icon3');
 const textMenu = document.querySelector('.links');
-
-iconMenu.addEventListener('click', () => {  
-  let positionMenu = document.defaultView.getComputedStyle(textMenu, null).right;
-
-  if(positionMenu == "-180px"){
-    textMenu.style.right = "0px"
-    iconMenu.classList.add("open");
-
-  }
-   else {
+let windowWidth;
+window.addEventListener('resize', function(){
+  windowWidth = window.innerWidth;
+  
+  if(windowWidth <= 800) {
     textMenu.style.right = "-180px"
+    
+    iconMenu.addEventListener('click', () => {  
+      let positionMenu = document.defaultView.getComputedStyle(textMenu, null).right;
+      
+      if(positionMenu == "-180px"){
+        textMenu.style.right = "0px"
+        iconMenu.classList.add("open");
+      }
+      else {
+        textMenu.style.right = "-180px"
+        iconMenu.classList.remove("open");
+      }
+    })
+    
+  } else {
+    textMenu.style.right = "0px"
     iconMenu.classList.remove("open");
+  }  
+});
 
-  }
-})
+
+
