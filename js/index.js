45f0e7d6c24140179b4d7162b6cd45a5871f8f7e
Anderson Garcia <andersongarciapinheiro@gmail.com>
2020-02-27 16:11:20 -0300

    bg carrosel #1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
+let getDom = el => {
+  let elemento = document.querySelector(el)
+  return elemento
+}
 
+let bgs = ['setBg-a', 'setBg-b', 'setBg-c']
+
+getDom(".right").onclick = () => {
+  getDom(".carrosel-img1").classList.add(bgs[1])
+}
+
+getDom(".left").onclick = () => {
+  getDom(".carrosel-img1").classList.add(bgs[0])
+}
+
+
+// function bgRight() {
+
+// }
+
+
+
+
+console.log(bgs.map)
