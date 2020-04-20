cda945e2305cd7812045cbc0f69213e4c43dceb9
Anderson Garcia <50424643+andersongarciapinheiro@users.noreply.github.com>
2020-04-20 17:57:18 -0300

    animation title and pharagraph

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ let carrosel = getDom(".carrosel-img1")
 let rightButton = getDom(".right")
 let leftButton = getDom(".left")
 let line = document.querySelectorAll(".menu-line")
+let bannerText = document.querySelectorAll(".banner-text")
 
 
 rightButton.onclick = rightClick
@@ -24,19 +25,23 @@ function leftClick() {
 }
 
 function clickRight() {
+  bannerText[index].classList.remove("show-display")
   line[index].classList.remove("line-selected")
   carrosel.classList.remove(bgs[index])
   index = index === qtdBg ? 0 : index + 1
   carrosel.classList.add(bgs[index])
   line[index].classList.add("line-selected")
+  bannerText[index].classList.add("show-display")
 }
 
 function clickLeft() {
+  bannerText[index].classList.remove("show-display")
   line[index].classList.remove("line-selected")
   carrosel.classList.remove(bgs[index])
   index = index === 0 ? qtdBg : index - 1
   carrosel.classList.add(bgs[index])
   line[index].classList.add("line-selected")
+  bannerText[index].classList.add("show-display")
 }
 
 function clickDisable() {
